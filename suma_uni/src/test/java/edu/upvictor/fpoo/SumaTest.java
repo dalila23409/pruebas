0e2e6ae50a9b560102cf7a864d0f5651cd8ac3ef
xmoren025 <2130293@upv.edu.mx>
2024-02-27 08:42:49 -0800

    Testing

--- suma_uni/src/test/java/edu/upvictor/fpoo/SumaTest.java
+++ suma_uni/src/test/java/edu/upvictor/fpoo/SumaTest.java
@@ -1,5 +1,14 @@
 package edu.upvictor.fpoo;
 
-public class SumaTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class SumaTest {
+    @Test
+    public void sumar_enteros(){
+        Suma s = new Suma();
+
+        assertEquals(4,s.suma(2,2));
+    }
 }
