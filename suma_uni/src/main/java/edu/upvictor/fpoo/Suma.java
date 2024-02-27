0df9ed240976b425aa2e85fa36004654d409990b
Deisy Dalila De La Fuente Alvarado <2230162@upv.edu.mx>
2024-02-27 10:21:05 -0600

    primero

---
+++ suma_uni/src/main/java/edu/upvictor/fpoo/Suma.java
@@ -0,0 +1,10 @@
+package edu.upvictor.fpoo;
+
+public class Suma {
+    public Suma (){
+
+    }
+    public int suma (int a, int b){
+        return 0;
+    }
+}
---
+++ suma_uni/src/test/java/edu/upvictor/fpoo/SumaTest.java
@@ -0,0 +1,5 @@
+package edu.upvictor.fpoo;
+
+public class SumaTest {
+
+}
